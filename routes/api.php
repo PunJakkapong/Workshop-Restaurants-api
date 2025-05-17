366734291edf59d40a9f014a4b031aa988a4cd51
PunJakkapong <jakkapong@goship.co.th>
2025-05-17 15:03:34 +0700

    add api route /restaurants and /photo

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\RestaurantController;
+use App\Http\Controllers\Api\PhotoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,3 +23,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::get('/ping', function () {
     return response()->json(['message' => 'pong']);
 });
+
+// Restaurant endpoints - returns list of all restaurants
+Route::get('/restaurants', [RestaurantController::class, 'index']);
+
+// Photo endpoints - returns image buffer
+Route::get('/photo', [PhotoController::class, 'index']);
